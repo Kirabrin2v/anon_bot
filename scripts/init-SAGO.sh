77cb0a0ae769b6e52c8266ce180515ddc057bd6a
Kirabrin2v <petrovkirill4187@gmail.com>
2026-02-14 18:14:14 +0300

    Add docker script

--- scripts/init-SAGO.sh
+++ scripts/init-SAGO.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Создаёт файл, необходимый для работы модуля SAGO
 
-echo '[informed_users]\ngrief_alert=[' > modules/SAGO/permanent_memory.ini
+echo '[informed_users]\ngrief_alert=[]' > modules/SAGO/permanent_memory.ini
---
+++ scripts/init-gpt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Создаёт конфиг для модуля gpt
+
+echo '[AI]\ngpt_bearer_token = *ВАШ ТОКЕН*' > modules/gpt/config.ini
