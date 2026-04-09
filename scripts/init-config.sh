04d66e96abacf05344fe0d8a205595f332a379cc
Kirabrin2v <petrovkirill4187@gmail.com>
2026-04-09 23:25:01 +0300

    Edit config.ini

--- scripts/init-config.sh
+++ scripts/init-config.sh
@@ -2,11 +2,35 @@
 # Создаёт файл с настройками
 
 echo "[VARIABLES]
-price_TCA = 10000
+host = mnrt.teslacraft.org
+port = 25565
+
 #Порт для отслежвания состояния клавиатуры/мыши
 port_keyboard_event = 8800
+active_nick = *НИК_БОТА*
 
-#Вставить свои данные
-[bot_nick]
-bot_password = bot_password
-bot_pin = bot_pin" > txt/config.ini
+interval_send_cmds = 900
+interval_check_surv = 5000
+
+seniors = []
+masters = []
+master_cmds = ["/swarp", "/warp", "/top"]
+ignore_cmds = ["/tca log", "/tca check", "/bal", "/seen"]
+
+#Сервер для РУЧУП
+run_local_server = False
+
+[TESLA]
+ranks = [null, "Рядовой", "Ефрейтор", "Мл. Сержант", "Сержант", "Ст. Сержант", "Прапорщик", "Ст. Прапорщик", "Лейтенант", "Ст. Лейтенант", "Капитан", "Майор", "Подполковник", "Полковник", "Генерал", "Маршал", "Император"]
+price_TCA = 10000
+
+[PROXY]
+host = *IP*
+port = *PORT*
+login = *LOGIN*
+password = *PASSWORD*
+
+[*НИК_БОТА*]
+bot_password = *ПАРОЛЬ*
+bot_pin = *ПИН-КОД*
+" > txt/config.ini
